7d7e23a862990f28ed7529fbc2f77fd9847bf909
johnmarion1126 <48599206+johnmarion1126@users.noreply.github.com>
2021-12-31 15:35:54 -0800

    Add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header': '#ffffff',
+        'text': '#dddcdf',
+        'link': '#606873',
+        'socials': '#c4c4c4',
+        'background': '#1f1f24'
+      },
+      fontFamily: {
+        'header': ['Raleway'],
+        'text': ['Open Sans']
+      }
+    },
   },
   plugins: [],
 }
